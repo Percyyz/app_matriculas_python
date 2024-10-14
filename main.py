9153e8cfe89b0d661135e59a974a0e144c01dc6a
percyz30 <percynoeyarihuaman@gmail.com>
2024-10-14 10:19:18 -0500

    actualizando

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-# """
-# - Registar alumnos.
-# - Generar fichas de matricula.
-# - Mostrar la lista de todos los matriculados.
-# - Filtrar matriculados por programa de estudios.
-# """
+"""
+- Registar alumnos.
+- Generar fichas de matricula.
+- Mostrar la lista de todos los matriculados.
+- Filtrar matriculados por programa de estudios.
+"""
 
 
-# cantidad_alumnos = int(input("¿Cuántos alumnos deseas ingresar? "))
-# lista_alumnos = []
-# for i in range(cantidad_alumnos):
-#     nombre = input(f"Ingresa el nombre del alumno {i+1}: ")
-#     lista_alumnos.append(nombre)
-# print("Lista de alumnos ingresada:", lista_alumnos)
+cantidad_alumnos = int(input("¿Cuántos alumnos deseas ingresar? "))
+lista_alumnos = []
+for i in range(cantidad_alumnos):
+    nombre = input(f"Ingresa el nombre del alumno {i+1}: ")
+    lista_alumnos.append(nombre)
+print("Lista de alumnos ingresada:", lista_alumnos)
 
 
-# lista_alumnos=[]
-# while True:
-#     opcion=input("""Elije lo que decea hacer
-#         escribe (s) si decea registrar un alumno
-#         escrbe (n) si desea salir del programa
-#         escribe tu respuersta: """)
-#     if opcion.lower=="n":
-#         break
-#     nombre=input("ingrese el ombre del alumno: ")
-#     apellido=input("ingrese apellido del alumno: ")
+lista_alumnos=[]
+while True:
+    opcion=input("""Elije lo que decea hacer
+        escribe (s) si decea registrar un alumno
+        escrbe (n) si desea salir del programa
+        escribe tu respuersta: """)
+    if opcion.lower=="n":
+        break
+    nombre=input("ingrese el ombre del alumno: ")
+    apellido=input("ingrese apellido del alumno: ")
 
-#     alumno={
-#         "nombre":nombre,
-#         "apellido":apellido
-#     }
-# lista_alumnos.append(alumno)
-# print(lista_alumnos)
+    alumno={
+        "nombre":nombre,
+        "apellido":apellido
+    }
+lista_alumnos.append(alumno)
+print(lista_alumnos)
 
 # |----------------------------------------------------------ejemplo realizado en el salon-------------------------------------------------------------|
 lista_alumnos=[]
